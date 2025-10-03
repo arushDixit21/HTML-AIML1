fd243d5c04e63367407fe1b03457e796ae768334
arushDixit21 <arushdixitapsb@gmail.com>
2025-10-03 15:16:34 +0530

    basic elements

---
+++ .vscode/new.cpp
@@ -0,0 +1,11 @@
+# include<iostream> 
+ using namespace std
+ int main ();
+{ 
+int a=10;
+double b=3.14;
+char C='A';
+cout<<typeid(a).name()<<endl;
+cout<<typeid(b).name()<<endl;
+cout<<typeid(c).name()<<endl;
+return0;}
